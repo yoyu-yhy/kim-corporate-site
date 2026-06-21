2f1a56aa0fa69434c055f5bb1f43f0090375c7e4
yoyu-yhy <yhyltd888828@gmail.com>
2026-06-21 22:58:03 +0900

    アイコン追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
